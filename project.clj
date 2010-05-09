de6df1a0dba997914971e5c4ae7b220d27691b4b
Rayne <DiscipleRayne@gmail.com>
2010-05-09 14:57:37 -0500

    * project.clj: Added commons-lang dep for StringEscapeUtils.

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
                  [org.clojure/clojure-contrib "1.1.0"]
 		 [net.cgrand/moustache "1.0.0-SNAPSHOT"]
                  [ring/ring-jetty-adapter "0.2.0"]
+		 [commons-lang/commons-lang "2.5"]
 		 [clj-sandbox "0.3.4"]
 		 [hiccup "0.2.3"]]
   :dev-dependencies [[swank-clojure "1.2.0-SNAPSHOT"]
