0e3a548bf6313c788146c7e402911836b32e9420
Heinz N. Gies <heinz@licenser.net>
2010-05-20 02:52:49 +0200

    Fixed analytics code.

--- src/tryclojure/core.clj
+++ src/tryclojure/core.clj
@@ -93,10 +93,17 @@
 			     "their results right here in your browser. We also have a brief tutorial to give you a "
 			     "taste of Clojure. Try it out!"]]]]]]
       [:script {:type "text/javascript"}
-       "try {
-  var pageTracker = _gat._getTracker('UA-552543-3');
-  pageTracker._trackPageview();
-} catch(err) {}"]))
+       "
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-552543-3']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+"]))
 
 (defn handler [{session :session}]
   {:status  200
