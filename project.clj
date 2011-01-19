024895ceaf14b13abb9a10639f9dc65549232a05
Raynes <i@raynes.me>
2011-01-19 11:02:07 -0600

    Exclude buggy dependency and update clojail dep.

--- project.clj
+++ project.clj
@@ -2,11 +2,10 @@
   :description "A simple web-based Clojure REPL for trying out Clojure without having to install it."
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-		 [net.cgrand/moustache "1.0.0-SNAPSHOT"]
+		 [net.cgrand/moustache "1.0.0-SNAPSHOT" :exclusions [org.clojure/clojure]]
                  [ring "0.3.3"]
 		 [commons-lang/commons-lang "2.5"]
-                 [clojail "0.2.2-SNAPSHOT"]
-		 [org.clojars.rayne/clj-sandbox "0.4.2-SNAPSHOT"]
+                 [clojail "0.3.4-SNAPSHOT"]
                  [org.clojars.rayne/clj-highlight "1.1.1-SNAPSHOT"]
 		 [hiccup "0.2.7"]]
   :dev-dependencies [[swank-clojure "1.2.1"]
