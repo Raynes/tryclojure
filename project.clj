88629c79c16c0349c49801bf598067874288658c
Anthony Grimes <i@raynes.me>
2011-10-31 21:04:46 -0500

    Update to newest clojail.

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [noir "1.2.0"]
 		 [commons-lang/commons-lang "2.5"]
-                 [clojail "0.4.6-beta5"]]
+                 [clojail "0.4.6-beta6"]]
   ;; For lein and Heroku compatibility. If you're using cake, add
   ;; this line to .cake/config: jvm.opts = -Djava.security.policy=example.policy
   :jvm-opts ["-Djava.security.policy=example.policy"]
