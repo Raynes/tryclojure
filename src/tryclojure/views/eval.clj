8e132f046391b1c5fb2a383154f02ccf2f4a35ad
Anthony Grimes <i@raynes.me>
2011-11-29 10:06:38 -0600

    Use jsonp from noir.response.

--- src/tryclojure/views/eval.clj
+++ src/tryclojure/views/eval.clj
@@ -1,16 +1,16 @@
 (ns tryclojure.views.eval
   (:use [noir.core :only [defpage]]
-        [noir.response :only [json]]
-        [tryclojure.models.eval :only [eval-request]]))
+        [tryclojure.models.eval :only [eval-request]])
+  (:require [noir.response :as resp]))
 
 (defpage "/eval.json" {:keys [expr jsonp]}
-  (update-in
-   (json
-    (let [{:keys [expr result error message] :as res} (eval-request expr)]
-      (if error
-        res
-        (let [[out res] result]
-          {:expr (pr-str expr)
-           :result (str out (pr-str res))}))))
-   [:body]
-   #(if jsonp (str jsonp "(" % ")") %)))
+  (let [{:keys [expr result error message] :as res} (eval-request expr)
+        data (if error
+               res
+               (let [[out res] result]
+                 {:expr (pr-str expr)
+                  :result (str out (pr-str res))}))]
+    
+    (if jsonp
+      (resp/jsonp jsonp data)
+      (resp/json data))))
