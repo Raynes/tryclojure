098dbb9381a3db79d1e2d32895bccef4e6f11525
Michael Klishin <michael@defprotocol.org>
2013-03-15 22:18:54 +0400

    Link to clojure-doc.org

--- src/tryclojure/views/home.clj
+++ src/tryclojure/views/home.clj
@@ -6,9 +6,8 @@
 (defpartial links-html []
   (unordered-list
    [(link-to "http://clojure.org" "The official Clojure website")
-    (link-to "http://dev.clojure.org/display/doc/Getting+Started" "Getting started with Clojure")
+    (link-to "http://clojure-doc.org/" "Clojure tutorials and documentation")
     (link-to "http://groups.google.com/group/clojure" "Clojure mailing list")
-    (link-to "http://java.ociweb.com/mark/clojure/article.html" "A comprehensive Clojure tutorial")
     (link-to "http://joyofclojure.com/" "The Joy of Clojure: a book by Michael Fogus and Chris Houser")
     (link-to "http://disclojure.org" "Disclojure")
     (link-to "http://planet.clojure.in" "Planet Clojure")]))
