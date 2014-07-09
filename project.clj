5b06fb733b63969fa4db290e0657ee6eb6b6d2e5
Ray Miller <ray@1729.org.uk>
2014-07-09 21:12:53 +0100

    Bump dependency versions; minor corrections to tutorial text.

--- project.clj
+++ project.clj
@@ -2,14 +2,14 @@
   :description "A simple web-based Clojure REPL for trying out Clojure without having to install it."
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [enlive "1.1.5"]
-                 [lib-noir "0.8.1"]
-                 [compojure "1.1.6"]
+                 [lib-noir "0.8.4"]
+                 [compojure "1.1.8"]
                  [ring-server "0.3.1"]
                  [commons-lang/commons-lang "2.5"]
                  [clojail "1.0.6"]]
   :jvm-opts ["-Djava.security.policy=example.policy" "-Xmx80M"]
   :min-lein-version "2.0.0"
   :uberjar-name "tryclojure-standalone.jar"
-  :plugins [[lein-ring "0.8.10"]]
+  :plugins [[lein-ring "0.8.11"]]
   :ring {:handler tryclojure.server/app :port 8801}
   :profiles {:dev {:ring {:nrepl? true}}})
