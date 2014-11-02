b00e6688325ad34e61df1c45e3b69392b952ed6e
Michael Fogleman <michaelwfogleman@gmail.com>
2014-11-02 17:30:51 -0500

    Update project dependencies, including Clojure.

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
 (defproject tryclojure "0.1.0-SNAPSHOT"
   :description "A simple web-based Clojure REPL for trying out Clojure without having to install it."
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [lib-noir "0.8.1"]
-                 [compojure "1.1.6"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [lib-noir "0.9.4"]
+                 [compojure "1.2.1"]
                  [ring-server "0.3.1"]
-                 [commons-lang/commons-lang "2.5"]
+                 [commons-lang/commons-lang "2.6"]
                  [clojail "1.0.6"]]
   :jvm-opts ["-Djava.security.policy=example.policy" "-Xmx80M"]
   :min-lein-version "2.0.0"
